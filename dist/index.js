e02be4d1bc3d04b8ecb3fae74b3296553e69d614
yona3 <kou.y.0905@icloud.com>
2021-07-25 23:10:24 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var msg = "Hello";
+console.log(msg);
+//# sourceMappingURL=index.js.map
---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+import { sum } from "./index";
+
+test("sum test", () => {
+  expect(sum(1, 2)).toBe(3);
+});
